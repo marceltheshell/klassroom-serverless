17ad0fc2151d3ce7b6e1416229375943871459bb
degasm <marcel.degas@autodesk.com>
2020-12-28 13:18:42 -0800

    adds routers to services

---
+++ lambdas/klassroom/lib/teachers.js
@@ -0,0 +1,21 @@
+function router(event) {
+    const method = event.method;
+    console.log('The incoming method is:', method);
+    
+    switch(method) {
+        case '/GET':
+            //
+        case '/POST':
+            //
+        case '/PUT':
+            //
+        case '/DELETE':
+            //
+        default:
+            return response.buildResponse(500, 'Method was not recognized')
+    }
+}
+
+module.exports = {
+    router
+}
